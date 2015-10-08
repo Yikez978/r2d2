777d9f10d7942f34d708e91ec28b545f4f72e42b
Eric McCullough <eric_mccullough@hotmail.com>
2015-10-07 23:02:46 -0400

    Add another Sweep spec.

--- gui/spec/models/sweep_spec.rb
+++ gui/spec/models/sweep_spec.rb
@@ -1,8 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Sweep, type: :model do
-  it "is invalid if empty" do
+  it 'is invalid if description is empty' do
     sweep = Sweep.new()
     expect(sweep).to be_invalid
+  end
+  it 'is valid if description is not empty' do
+    sweep = Sweep.new(description: '192.168.1.0/24')
+    expect(sweep).to be_valid
   end
 end
