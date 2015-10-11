391adbf7cc8b2f7361364c710d00f36b38a990a1
Eric McCullough <eric_mccullough@hotmail.com>
2015-10-11 13:04:35 -0400

    Whitespace only.

--- gui/app/models/device.rb
+++ gui/app/models/device.rb
@@ -4,6 +4,6 @@ class Device < ActiveRecord::Base
   self.per_page = 10
   VALID_MAC_REGEX = /\A[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}:[\da-f]{2}\z/i
   validates :mac, presence: true,
-                  format: { with: VALID_MAC_REGEX }                 
+                  format: { with: VALID_MAC_REGEX }
   validates :ip, presence: true
 end
