ef99d9e5d853c0d2d1022620ed04d4902be42929
Eric McCullough <eric_mccullough@hotmail.com>
2016-05-27 10:24:22 -0400

    Add mask required to Scope
    and IP address validation

--- gui/app/models/scope.rb
+++ gui/app/models/scope.rb
@@ -1,6 +1,10 @@
 class Scope < ActiveRecord::Base
+  require 'resolv'
   validates :ip, presence: true,
-                 uniqueness: true
+                 uniqueness: true,
+                 :format => { :with => Resolv::IPv4::Regex }
+  validates :mask, presence: true,
+                   :format => { :with => Resolv::IPv4::Regex }
   has_many :leases
   belongs_to :server
   accepts_nested_attributes_for :leases
--- gui/spec/models/scope_spec.rb
+++ gui/spec/models/scope_spec.rb
@@ -1,18 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Scope, type: :model do
-  it 'is invalid if IP is empty' do
-    scope = Scope.new()
-    expect(scope).to be_invalid
+  describe 'is invalid' do
+    it 'if IP is not defined' do
+      scope = Scope.new(mask: '255.0.0.0')
+      expect(scope).to be_invalid
+    end
+    it 'if the mask is not defined' do
+      scope = Scope.new(ip: '1.1.1.1')
+      expect(scope).to be_invalid
+    end
+    it 'if the IP is not valid' do
+      scope = Scope.new(ip: '355.1.1.1', mask: '255.0.0.0')
+      expect(scope).to be_invalid
+    end
+    it 'if the mask is not valid' do
+      scope = Scope.new(ip: '1.1.1.1', mask: '355.0.0.0')
+      expect(scope).to be_invalid
+    end   
   end
-  it 'is valid if IP is not empty' do
-    scope = Scope.new(ip: '1.1.1.1')
-    expect(scope).to be_valid
-  end
-  it 'the IP is unique' do
-    scope1 = Scope.create(ip: '1.1.1.1')
-    scope2 = Scope.new(ip: '1.1.1.1')
-    expect(scope1).to be_valid
-    expect(scope2).to be_invalid
+  describe 'is valid' do
+    it 'if IP and mask are defined' do
+      scope = Scope.new(ip: '1.1.1.1', mask: '255.0.0.0')
+      expect(scope).to be_valid
+    end
+    it 'if the IP is unique' do
+      scope1 = Scope.create(ip: '1.1.1.1', mask: '255.0.0.0')
+      scope2 = Scope.new(ip: '1.1.1.1', mask: '255.255.0.0')
+      expect(scope1).to be_valid
+      expect(scope2).to be_invalid
+    end
   end
 end
